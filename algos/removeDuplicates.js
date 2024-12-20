8c5cd2e9ecddc4d50cb11b11dc29a51cd9f301ec
Beto Carlos <me@beto.codes>
2024-12-20 15:53:56 -0700

    Adds remove duplicates

--- algos/removeDuplicates.js
+++ algos/removeDuplicates.js
@@ -1,7 +1,25 @@
-// With Set - O(n)
+// Set - O(n)
 const removeDuplicates = (arr) => {
 	const set = new Set(arr);
 	return [...set];
 };
 
-console.log(removeDuplicates([1, 2, 3, 3, 2, 5, 6, 7, 9]));
+// Object tracking - O(n)
+// const removeDuplicates = (arr) => {
+// 	const uniqueArray = [];
+// 	const seen = {};
+// 	for (const item of arr) {
+// 		if (!seen[item]) {
+// 			seen[item] = true;
+// 			uniqueArray.push(item);
+// 		}
+// 	}
+// 	return uniqueArray;
+// };
+
+// indexOf - O(n^2)
+// const removeDuplicates = (arr) => {
+// 	return arr.filter((item, index) => arr.indexOf(item) === index);
+// };
+
+console.log(removeDuplicates([1, 2, 3, 3, 2, 5, 6, 7, 9, 5, 6, 3, 8, 6, 3, 9]));
