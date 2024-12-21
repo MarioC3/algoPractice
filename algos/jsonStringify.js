234088d2b7eea60ea952f03f96c5571e1918a7a4
Beto Carlos <me@beto.codes>
2024-12-20 19:06:14 -0700

    handle more types

--- algos/jsonStringify.js
+++ algos/jsonStringify.js
@@ -1,13 +1,31 @@
 const jsonStringify = (val) => {
 	if (Array.isArray(val)) {
-		const arrayVals = val.map((item) => jsonStringify(item));
+		const arrayVals = val.map((item) => {
+			if (
+				item === undefined ||
+				typeof item === "function" ||
+				typeof item === "symbol"
+			) {
+				return String(null);
+			} else {
+				jsonStringify(item);
+			}
+		});
 		return `[${arrayVals.join(",")}]`;
 	}
 
 	if (typeof val === "object" && val !== null) {
-		const objEntries = Object.entries(val).map(
-			([objKey, objVal]) => `"${objKey}":${jsonStringify(objVal)}`
-		);
+		const objEntries = Object.entries(val).map(([objKey, objVal]) => {
+			if (typeof objKey === "string") {
+				if (
+					objVal !== undefined &&
+					typeof objVal !== "function" &&
+					typeof objVal !== "symbol"
+				) {
+					return `"${objKey}":${jsonStringify(objVal)}`;
+				}
+			}
+		});
 
 		return `{${objEntries.join(",")}}`;
 	}
@@ -20,9 +38,25 @@ const jsonStringify = (val) => {
 		return String(null);
 	}
 
+	if (typeof val === "bigint") {
+		throw new TypeError("Do not know how to serialize a BigInt");
+	}
+
+	if (
+		typeof val === "function" ||
+		typeof val === "symbol" ||
+		value === undefined
+	) {
+		return undefined;
+	}
+
+	if (val instanceof Map || val instanceof Set) {
+		return String({});
+	}
+
 	return String(val);
 };
 
-const value = NaN;
+const value = Symbol("");
 const result = jsonStringify(value);
 console.log(result);
